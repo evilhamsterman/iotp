cb94fb2359bcd0daa65585db3eb1f4c08065d39c
Dan Mills <evilhamsterman@gmail.com>
2016-05-26 14:27:26 -0700

    Add license and classifiers for PyPi

--- setup.py
+++ setup.py
@@ -4,6 +4,19 @@ setup(
     name='iotp',
     author='Dan Mills',
     version='0.0.2',
+    license='Apache 2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
     description='A CLI app for TOTP',
     py_modules=['iotp'],
     install_requires=[
